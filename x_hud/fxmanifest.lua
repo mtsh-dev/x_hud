a93057788aa4b7b5801232f7e75a118467c6b8a8
xad420 <98492604+xad420@users.noreply.github.com>
2023-03-18 00:35:12 +0100

    Update fxmanifest.lua

--- x_hud/fxmanifest.lua
+++ x_hud/fxmanifest.lua
@@ -9,7 +9,6 @@ description 'hud do chuja nie podobny uzywasz na własną odpowiedzialnosc :D'
 author 'xad'
 version '1.0.0'
 
-shared_script '@es_extended/imports.lua'
 
 server_scripts {
     'server/main.lua'
